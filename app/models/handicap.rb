90607afd362b103083ae7676224f3eab7fe3c57c
Peter Baggish <p.baggish@gmail.com>
2016-08-13 10:18:08 -0400

    avatars fixed, profile Edit fixed, Landing page partially styled

--- app/models/handicap.rb
+++ app/models/handicap.rb
@@ -1,5 +1,6 @@
 class Handicap < ActiveRecord::Base
   belongs_to :user
+  validates :score, :course_rating, :course_slope_rating, presence: true
 
  def self.my_handicaps(user)
    where(user_id: user.id)
