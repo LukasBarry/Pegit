8542d4a1fe1ba5de117b99d20ba752202b9777a9
Bake <bakerst910@gmail.com>
2016-08-15 12:30:13 -0400

    landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   get 'profiles/:id' => 'profiles#show', as: :profile
   root 'handicaps#index'
   get 'feed' => 'meetups#feed'
-  resources :meetups do
+    resources :meetups do
     resource :partner, except: [:show]
   end
-  resources :handicaps
-  devise_for :users
+    resources :handicaps
+    devise_for :users
 
 end
